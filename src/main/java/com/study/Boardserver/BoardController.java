1a7f8777cc8a56746b82b6f3e66bd13e882fe79c
jyj1289 <jin37091289@gmail.com>
2023-07-08 21:45:32 +0900

    feat : findOne Controller

--- src/main/java/com/study/Boardserver/BoardController.java
+++ src/main/java/com/study/Boardserver/BoardController.java
@@ -1,9 +1,7 @@
 package com.study.Boardserver;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +11,10 @@ public class BoardController {
     @PostMapping("/board/create")
     public BoardDto create(@RequestBody BoardDto boardDto){
         return boardService.create(boardDto);
+    }
+
+    @GetMapping("/board/find/{id}")
+    public BoardDto findOne(@PathVariable Long id){
+        return boardService.findOne(id);
     }
 }
