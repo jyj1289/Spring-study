471eda81b74a3239fd6f5d17cfb2a22c227d8c65
jyj1289 <jin37091289@gmail.com>
2023-07-08 20:10:31 +0900

    initial commit

---
+++ src/main/java/com/study/Boardserver/Board.java
@@ -0,0 +1,26 @@
+package com.study.Boardserver;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Board {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Board(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
