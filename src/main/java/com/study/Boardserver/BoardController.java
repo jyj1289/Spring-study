b042b609e67aedd2036c7719a8f62f1cca6c1c3b
jyj1289 <jin37091289@gmail.com>
2023-07-08 21:43:25 +0900

    feat : BoardController

---
+++ src/main/java/com/study/Boardserver/BoardController.java
@@ -0,0 +1,10 @@
+package com.study.Boardserver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+}
