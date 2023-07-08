40d63908cb2809191462c0cd17f48674f23b2b90
jyj1289 <jin37091289@gmail.com>
2023-07-08 21:44:49 +0900

    feat : create Controller

--- src/main/java/com/study/Boardserver/BoardController.java
+++ src/main/java/com/study/Boardserver/BoardController.java
@@ -1,10 +1,17 @@
 package com.study.Boardserver;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @PostMapping("/board/create")
+    public BoardDto create(@RequestBody BoardDto boardDto){
+        return boardService.create(boardDto);
+    }
 }
