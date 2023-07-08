dcec58e577adac9f7812114321e3a7a86e626c76
jyj1289 <jin37091289@gmail.com>
2023-07-08 20:20:32 +0900

    feat : create board service

--- src/main/java/com/study/Boardserver/BoardService.java
+++ src/main/java/com/study/Boardserver/BoardService.java
@@ -1,4 +1,20 @@
 package com.study.Boardserver;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardDto create(BoardDto boardDto){
+        Board board = new Board(boardDto.getTitle(), boardDto.getContent());
+
+        Board save = boardRepository.save(board);
+
+        return new BoardDto(save);
+    }
 }
